1f3ea1813a5de38a31b6b727f6421f7e63ec1448
mhogg <mhogg@student.21-school.ru>
2022-09-23 20:21:19 +0500

    done tags

--- rush00/tags.py
+++ rush00/tags.py
@@ -1,10 +1,18 @@
-# irina
+#irina
 import re
 from collections import Counter
 
-
 class Tags:
-    # Analyzing data from tags.csv
+    """
+    Analyzing data from tags.csv
+    """
+
+    # def __init__(self, path_to_the_file):
+    #     try:
+    #         with open(path_to_the_file, 'r') as file:
+    #             self.file_data = file.readlines()
+    #     except FileNotFoundError as err:
+    #         print(err)
 
     def __init__(self, path_to_the_file, has_order=True):
         self.path_file = path_to_the_file
@@ -39,11 +47,12 @@ class Tags:
         text = text.replace('/', ' ')  # removing '/'
         return text
 
-
-    # The method returns top-n tags with most words inside. It is a dict
-    # where the keys are tags and the values are the number of words inside the tag.
-    # Drop the duplicates. Sort it by numbers descendingly.
     def most_words(self, n):
+        """
+        The method returns top-n tags with most words inside. It is a dict
+        where the keys are tags and the values are the number of words inside the tag.
+        Drop the duplicates. Sort it by numbers descendingly.
+        """
         tag_list = []
         lines = [row.split(',') for row in self.file_data[1:]]
         for line in lines:
@@ -54,9 +63,11 @@ class Tags:
         big_tags = dict(tag_list[:n])
         return big_tags
 
-    # The method returns top-n longest tags in terms of the number of characters.
-    # It is a list of the tags. Drop the duplicates. Sort it by numbers descendingly.
     def longest(self, n):
+        """
+        The method returns top-n longest tags in terms of the number of characters.
+        It is a list of the tags. Drop the duplicates. Sort it by numbers descendingly.
+        """
         tag_list = []
         lines = [row.split(',') for row in self.file_data[1:]]
         for line in lines:
@@ -68,29 +79,35 @@ class Tags:
         big_tags = list(tag_dict.keys())
         return big_tags
 
-    # The method returns the intersection between top-n tags with most words inside and
-    # top-n longest tags in terms of the number of characters.
-    # Drop the duplicates. It is a list of the tags.
     def most_words_and_longest(self, n):
+        """
+        The method returns the intersection between top-n tags with most words inside and
+        top-n longest tags in terms of the number of characters.
+        Drop the duplicates. It is a list of the tags.
+        """
         a = list(self.most_words(n).keys())
         b = self.longest(n)
         big_tags = list(set(a) & set(b))
         big_tags.sort(key=lambda x: len(x), reverse=True)
         return big_tags
 
-    # The method returns the most popular tags.
-    # It is a dict where the keys are tags and the values are the counts.
-    # Drop the duplicates. Sort it by counts descendingly.
     def most_popular(self, n):
+        """
+        The method returns the most popular tags.
+        It is a dict where the keys are tags and the values are the counts.
+        Drop the duplicates. Sort it by counts descendingly.
+        """
         lines = [row.split(',') for row in self.file_data[1:]]
         tags = [line[2] for line in lines]
         cnt = Counter(tags)
         popular_tags = dict(cnt.most_common(n))
         return popular_tags
 
-    # The method returns all unique tags that include the word given as the argument.
-    # Drop the duplicates. It is a list of the tags. Sort it by tag names alphabetically.
     def tags_with(self, word):
+        """
+        The method returns all unique tags that include the word given as the argument.
+        Drop the duplicates. It is a list of the tags. Sort it by tag names alphabetically.
+        """
         lines = [row.split(',') for row in self.file_data[1:]]
         tag_list = [line[2] for line in lines if line[2].find(word) >= 0]
         # tag_list = [line[2] for line in lines]
@@ -99,4 +116,13 @@ class Tags:
         #     if re.findall(r'([W-w]ar)', tag):
         #         print(tag)
         tags_with_word = list(sorted(set(tag_list)))
+        # или спилитить и искать по отдельным словам?
         return tags_with_word
+
+if __name__ == '__main__':
+    t = Tags('../dataset/tags.csv')
+    print(t.most_words(10))
+    print(t.longest(10))
+    print(t.most_words_and_longest(10))
+    print(t.most_popular(10))
+    print(t.tags_with('war'))
